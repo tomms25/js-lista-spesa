dd9a2a75eb82c1ed9338437619ce8e1825a45e7b
tomms25 <tommasobaccano@gmail.com>
2022-11-14 16:54:52 +0100

    prove con for

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
+
+let listaSpesa = [],
+
+let item = false
+
+let listObject = prompt ("Inserisci elementi da aggiungere alla lista")
+
+for (let i = 0;   ; i++){
+    console.log(i)
+    if (listObject == listaSpesa [i]){
+        object = true;
+    }
+
+}
+
+// Da riportare in pagina
+
+if (object == true){
+    document.getElementById ("element").innerHTML = listObject
+} 
+  
